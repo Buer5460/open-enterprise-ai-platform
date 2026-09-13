c0b7ecc79b8193d76a0bf283db255796c992be3e
buer <zhuxiangbuer@gmail.com>
2026-09-13 08:53:17 +0800

    feat: export Package Manifest compatibility contract

--- packages/package-spec/src/index.ts
+++ packages/package-spec/src/index.ts
@@ -5,6 +5,7 @@ export * from "./workflow.js";
 export * from "./connector.js";
 export * from "./data-provider.js";
 export * from "./app.js";
+export * from "./compatibility.js";
 
 import type { OEAPSkillManifest } from "./skill.js";
 import type { OEAPAgentManifest } from "./agent.js";
